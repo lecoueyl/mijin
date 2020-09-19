53392d179c541a5c7c9db1747594769913c8112f
leo <leonard.lecouey@gmail.com>
2020-09-19 17:48:53 +0900

    Add theme switcher

--- doc/components/LayoutSidebar.vue
+++ doc/components/LayoutSidebar.vue
@@ -1,5 +1,13 @@
 <template>
   <aside>
+    <button
+      class="mb-10"
+      @click="switchTheme()"
+    >
+      <!-- eslint-disable-next-line -->
+      <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" /></svg>
+    </button>
+
     <section
       v-for="(section, sectionName, sectionIndex) in sections"
       :key="sectionName"
@@ -19,7 +27,7 @@
         >
           <nuxt-link
             :to="localePath(`${sectionName}-${menuName}`)"
-            class="hover:text-primary-500 transition-colors duration-100 ease-in-out px-2 py-1 block"
+            class="hover:text-primary-500 transition-colors duration-200 ease-in-out px-4 py-1 block"
             active-class="text-primary-700 hover:text-primary-700  bg-primary-200 rounded"
           >
             {{ $t(`sections.${sectionName}.${menuName}.title`) }}
@@ -36,6 +44,7 @@ import Vue from 'vue';
 export default Vue.extend({
   data() {
     return {
+      theme: 'light',
       // https://www.flexport.com/design/components
       // https://vercel.com/design/status-dot
       sections: {
@@ -51,6 +60,23 @@ export default Vue.extend({
         },
       },
     };
+  },
+
+  mounted() {
+    if (localStorage.getItem('theme')) this.theme = localStorage.getItem('theme');
+    this.setTheme();
+  },
+
+  methods: {
+    setTheme() {
+      document.documentElement.setAttribute('data-theme', this.theme);
+      localStorage.setItem('theme', this.theme);
+    },
+
+    switchTheme() {
+      this.theme = this.theme === 'light' ? 'dark' : 'light';
+      this.setTheme();
+    },
   },
 });
 </script>
