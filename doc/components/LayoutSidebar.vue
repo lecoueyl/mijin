9cfee4203044b748d6ebb00123ef9b7d2495bbef
leo <leonard.lecouey@gmail.com>
2020-09-19 18:10:30 +0900

    Add button theme

--- doc/components/LayoutSidebar.vue
+++ doc/components/LayoutSidebar.vue
@@ -1,11 +1,12 @@
 <template>
   <aside>
     <button
-      class="mb-10"
+      class="mb-10 bg-gray-200 hover:bg-gray-300 py-2 pl-2 pr-6 rounded-full inline-flex items-center transition-colors duration-200 ease-in-out"
       @click="switchTheme()"
     >
       <!-- eslint-disable-next-line -->
-      <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" /></svg>
+      <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" /></svg>
+      {{ theme }}
     </button>
 
     <section
