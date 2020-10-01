7d645bf3b02f61ef8d28315096f39ebe1262360e
leo <leonard.lecouey@gmail.com>
2020-10-01 21:36:42 +0900

    Add color

---
+++ doc/components/SampleColor.vue
@@ -0,0 +1,37 @@
+<template functional>
+  <div>
+    <div
+      v-for="(value, variation, index) in props.color"
+      :key="variation"
+      class="p-3"
+      :class="[
+        `bg-${props.name}-${variation}`,
+        parseInt(variation) < 500 ? 'text-black' : 'text-white',
+        {
+          'rounded-t-lg': index === 0,
+          'rounded-b-lg': index === Object.keys(props.color).length - 1,
+        },
+      ]"
+    >
+      {{ props.name }}-{{ variation }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    color: {
+      required: true,
+      type: Object,
+    },
+
+    name: {
+      required: true,
+      type: String,
+    },
+  },
+});
+</script>
--- doc/pages/foundations/colors.vue
+++ doc/pages/foundations/colors.vue
@@ -1,3 +1,74 @@
 <template>
-  <div />
+  <LayoutSample
+    :title="$t('sections.foundations.colors.title')"
+  >
+    <BbHeadline
+      id="Primary"
+      :level="2"
+      :size="3"
+      class="pb-8"
+    >
+      Primary
+    </BbHeadline>
+
+    <SampleColor
+      name="primary"
+      :color="colors.primary"
+    />
+
+    <BbHeadline
+      id="gray"
+      :level="2"
+      :size="3"
+      class="pt-8 pb-8"
+    >
+      Gray
+    </BbHeadline>
+
+    <SampleColor
+      name="gray"
+      :color="colors.gray"
+    />
+
+    <BbHeadline
+      id="accents"
+      :level="2"
+      :size="3"
+      class="pt-8"
+    >
+      Accents
+    </BbHeadline>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div
+        v-for="name in ['blue', 'green', 'yellow', 'red']"
+        :key="name"
+      >
+        <BbHeadline
+          :level="3"
+          :size="4"
+          class="py-8 capitalize"
+        >
+          {{ name }}
+        </BbHeadline>
+
+        <SampleColor
+          :name="name"
+          :color="colors[name]"
+        />
+      </div>
+    </div>
+  </LayoutSample>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import tailwindConfig from '../../../tailwind.config';
+
+export default Vue.extend({
+  data() {
+    return {
+      colors: tailwindConfig.theme.extend.colors,
+    };
+  },
+});
+</script>
