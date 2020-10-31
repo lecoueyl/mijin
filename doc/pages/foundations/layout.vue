837956b198bb65cd3655d79cfe03604fb0a00f94
leo <leonard.lecouey@gmail.com>
2020-10-31 16:34:08 +0900

    Add container

--- doc/pages/foundations/layout.vue
+++ doc/pages/foundations/layout.vue
@@ -3,8 +3,17 @@
     :title="$t('sections.foundations.colors.title')"
   >
     <Sample
-      :title="$t('props.default')"
-      :snippet="samples.default"
+      :title="$t('props.container')"
+      :snippet="samples.container"
+    >
+      <BbContainer>
+        <div class="py-10 bg-primary-500 rounded-md" />
+      </BbContainer>
+    </Sample>
+
+    <Sample
+      :title="$t('props.grid')"
+      :snippet="samples.grid"
     >
       <BbGrid cols="3" gap="4">
         <div class="py-10 bg-primary-500 rounded-md" />
@@ -22,7 +31,12 @@ export default Vue.extend({
   data() {
     return {
       samples: {
-        default: [
+        container: [
+          `<BbContainer>
+  <div class="py-10 bg-gray-300 rounded-md" />
+</BbContainer>`,
+        ],
+        grid: [
           `<BbGrid cols="3" gap="4">
   <div class="py-10 bg-gray-300 rounded-md" />
   <div class="py-10 bg-gray-300 rounded-md" />
