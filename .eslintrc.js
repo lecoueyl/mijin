4b97ec393b64f43f1031b02a9a8ce08758b583c4
leo <leonard.lecouey@gmail.com>
2021-01-07 17:45:54 +0900

    Add modal test

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
       files: [
         '**/__tests__/*.{j,t}s?(x)',
         '**/tests/unit/**/*.spec.{j,t}s?(x)',
+        '**/src/**/*.test.{j,t}s?(x)',
       ],
       env: {
         jest: true,
--- src/components/modal/modal.unit.test.js
+++ src/components/modal/modal.unit.test.js
@@ -10,16 +10,18 @@ describe('Modal', () => {
 
     expect(wrapper.element.tagName).toBe('DIV');
     expect(wrapper.classes('fixed')).toBe(true);
+    expect(wrapper.classes('invisible')).toBe(true);
+    expect(wrapper.vm.isOpen).toBe(false);
   });
 
   it('renders default slot content', async () => {
     const wrapper = shallowMount(Modal, {
       slots: {
-        default: '<section>foobar</section>',
+        default: '<span>foobar</span>',
       },
     });
 
-    expect(wrapper.find('section').exists()).toBe(true);
+    expect(wrapper.find('span').exists()).toBe(true);
     expect(wrapper.text()).toBe('foobar');
   });
 
@@ -32,4 +34,56 @@ describe('Modal', () => {
 
     expect(wrapper.element.tagName).toBe('DIV');
   });
+
+  it('open, close and toggle modal', async () => {
+    const wrapper = shallowMount(Modal);
+
+    await wrapper.vm.open();
+    expect(wrapper.vm.isOpen).toBe(true);
+    expect(wrapper.classes('visible')).toBe(true);
+    expect(wrapper.classes('invisible')).toBe(false);
+    expect(wrapper.emitted().open).toBeTruthy();
+
+    await wrapper.vm.close();
+    expect(wrapper.vm.isOpen).toBe(false);
+    expect(wrapper.classes('visible')).toBe(false);
+    expect(wrapper.classes('invisible')).toBe(true);
+    expect(wrapper.emitted().close).toBeTruthy();
+
+    await wrapper.vm.toggle();
+    expect(wrapper.vm.isOpen).toBe(true);
+    expect(wrapper.classes('visible')).toBe(true);
+    expect(wrapper.classes('invisible')).toBe(false);
+    expect(wrapper.emitted().toggle).toBeTruthy();
+  });
+
+  it('closes modal on escape key', async () => {
+    const wrapper = shallowMount(Modal);
+
+    await wrapper.vm.open();
+    expect(wrapper.vm.isOpen).toBe(true);
+    const keydownEnter = new KeyboardEvent('keydown', { keyCode: 13 });
+    await document.dispatchEvent(keydownEnter);
+    expect(wrapper.vm.isOpen).toBe(true);
+    const keydownEsc = new KeyboardEvent('keydown', { keyCode: 27 });
+    await document.dispatchEvent(keydownEsc);
+    expect(wrapper.vm.isOpen).toBe(false);
+  });
+
+  test.each([
+    ['base', 'sm:max-w-lg'],
+    ['lg', 'sm:max-w-3xl'],
+    ['full', 'w-full'],
+  ])(
+    'with prop size %p render class %p',
+    (size, expected) => {
+      const wrapper = shallowMount(Modal, {
+        propsData: {
+          size,
+        },
+      });
+
+      expect(wrapper.find('[role="dialog"]').classes(expected)).toBe(true);
+    },
+  );
 });
