2f06dcb77bcf281111e5eae9304fd96e75fe3f79
leo <leonard.lecouey@gmail.com>
2021-01-25 09:59:00 +0900

    test

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,5 @@
 export default {
   commitlint: {
     extends: ['@commitlint/config-conventional'],
-    rules: {
-      'subject-case': [2, 'never', ['start-case', 'pascal-case']],
-    },
   },
 };
