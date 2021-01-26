f84cb869a503f75a9671a926db3ef891b5bf8685
leo <leonard.lecouey@gmail.com>
2021-01-26 15:40:29 +0900

    fix(release): update semantic-release npm publish

--- release.config.js
+++ release.config.js
@@ -4,9 +4,7 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
-    ['@semantic-release/npm', {
-      npmPublish: false,
-    }],
+    '@semantic-release/npm',
     ['@semantic-release/changelog', {
       changelogFile: 'CHANGELOG.md',
     }],
