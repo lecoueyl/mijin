34f14f8157cb33e7fd2884a2ae03c9fa3b6c1e9b
leo <leonard.lecouey@gmail.com>
2021-01-29 11:48:25 +0900

    fix(icon): remove aria-labelledby

--- src/components/icon/Icon.vue
+++ src/components/icon/Icon.vue
@@ -1,6 +1,5 @@
 <template>
   <svg
-    :aria-labelledby="name"
     class="stroke-2"
     :class="{
       'h-3 w-3': size === 'sm',
@@ -10,7 +9,6 @@
     fill="none"
     stroke="currentColor"
     viewBox="0 0 24 24"
-    role="presentation"
     stroke-linecap="round"
     stroke-linejoin="round"
     xmlns="http://www.w3.org/2000/svg"
